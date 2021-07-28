8c722bcc9e5873d70d769325f3fdb306cf04ace9
Shea <galley.shea@gmail.com>
2021-07-27 20:02:22 -0400

    S4 78-84 | rotation of ship; laser PS; fire input

--- Argon_Assault/Assets/Scripts/PlayerControls.cs
+++ Argon_Assault/Assets/Scripts/PlayerControls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,21 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField] InputAction movement;
+    [SerializeField] InputAction fire;
     [SerializeField] float controlSpeed = 10f;
     [SerializeField] float xRange = 10f;
     [SerializeField] float YRange = 10f;
+    [SerializeField] GameObject[] lasers; //declare array of type Gameobject
+
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -20f;
+
+
+    float yThrow;
+    float xThrow;
+    float fireRate;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +31,33 @@ public class PlayerControls : MonoBehaviour
 
     void OnEnable() //OnEnable is a system function like start and update called between the two. https://docs.unity3d.com/Manual/ExecutionOrder.html
     {
+        fire.Enable();
+        movement.Enable(); //new input system needs to be explicitly enabled in OnEnable
         
-        {
-            movement.Enable(); //new input system needs to be explicitly enabled in OnEnable
-        }
     }
 
     void OnDisable()
     {
         movement.Disable();
+        fire.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float xThrow = movement.ReadValue<Vector2>().x;
-        float yThrow = movement.ReadValue<Vector2>().y;
+        ProcessTranslation();
+        ProcessRotation();
+        Attack();
+
+    }
+
+    void ProcessTranslation()
+    {
+        xThrow = movement.ReadValue<Vector2>().x; //from movement input system
+        yThrow = movement.ReadValue<Vector2>().y;
 
         // for new position we take local position, add movement input xThrow, control for time and multiply by control speed so it's not too slow.  do the same for Y
-        float newX = transform.localPosition.x + xThrow * Time.deltaTime * controlSpeed; 
+        float newX = transform.localPosition.x + xThrow * Time.deltaTime * controlSpeed;
         float newY = transform.localPosition.y + yThrow * Time.deltaTime * controlSpeed;
 
         //clamp x and Y values
@@ -45,6 +66,44 @@ public class PlayerControls : MonoBehaviour
 
         //use new position values in transform on every frame
         transform.localPosition = new Vector3(newXClamp, newYClamp, transform.localPosition.z);
+    }
 
+    void ProcessRotation()
+    {
+        float pitch = transform.localPosition.y * positionPitchFactor + yThrow * controlPitchFactor;
+        float yaw = transform.localPosition.x * positionYawFactor;
+        float roll = xThrow * controlRollFactor;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); //https://en.wikipedia.org/wiki/Quaternion
+    }
+
+    void Attack()
+    {
+       
+       if (fire.ReadValue<float>() > 0.5) //read value from input system
+        {
+            ActivateLasers();
+        }
+        else
+        {
+            DeactivateLasers();
+        }
+        
+    }
+
+    void DeactivateLasers()
+    {
+        foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(false);
+        }
+    }
+
+    void ActivateLasers()
+    {
+       foreach (GameObject laser in lasers)
+        {
+            laser.SetActive(true);
+        }
     }
 }
